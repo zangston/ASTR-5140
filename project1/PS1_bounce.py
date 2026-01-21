de11540e211d2879da199c4b6009ca7558c41678
Winston Zhang <winston.y.zhang@gmail.com>
2026-01-21 17:29:02 -0500

    Update PS1_bounce.py
    Add changes to ball behavior, see comments

--- project1/PS1_bounce.py
+++ project1/PS1_bounce.py
@@ -3,6 +3,12 @@
 # ORIGINAL FOUND AT:   http://python4astronomers.github.com/contest/bounce.html
 
 
+# Additions:
+#   - spawn 2 new balls every 10 frames
+#   - gradually speed up all balls over time by 2% compounding
+#   - randomize ball colors every frame
+#   - animation runs for 1 million frames instead of 500
+
 # Import and define some libraries
 import pylab
 import numpy as np
@@ -28,11 +34,44 @@ colors = np.random.random_sample([n, 4])
 # manipulation of the plotted circles.
 circles = pylab.scatter(pos[:,0], pos[:,1], marker='o', s=sizes, c=colors)
 
+spawn_every = 10        # spawn new balls every N frames
+spawn_count = 2         # how many to spawn each time
+max_balls = 10**6
+
+speedup_factor = 1.002  # speeds increase over time
+
 # Run animation
-for i in range(500):
+for i in range(10**6):
+
+    # spawn new balls
+    if (i % spawn_every == 0) and (n < max_balls):
+        k = min(spawn_count, max_balls - n)
+
+        new_pos = (20 * np.random.random_sample(k*2) - 10).reshape(k, 2)
+        new_vel = (0.6 * np.random.normal(size=k*2)).reshape(k, 2)
+        new_sizes = 100 * np.random.random_sample(k) + 100
+        new_colors = np.random.random_sample([k, 4])
+
+        pos = np.vstack([pos, new_pos])
+        vel = np.vstack([vel, new_vel])
+        sizes = np.concatenate([sizes, new_sizes])
+        colors = np.vstack([colors, new_colors])
+        n += k
+
+    vel *= speedup_factor   # speed up balls
+
     pos = pos + vel
     bounce = abs(pos) > 10      # Find balls that are outside walls
     vel[bounce] = -vel[bounce]  # Bounce if outside the walls
-    circles.set_offsets(pos)    # Change the positions
+    pos = np.clip(pos, -10, 10)
+
+    # vrandomize color every frame
+    colors[:, 0:3] = np.random.random_sample((n, 3))
+
+    # update plot
+    circles.set_offsets(pos)
+    circles.set_sizes(sizes)
+    circles.set_facecolors(colors)
+
     pylab.draw()
     pylab.pause(0.001)
